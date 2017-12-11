64b2eea82f2125a4b79fb8885bf9da210fca765c
raynerprogramming <raynerprogramming@gmail.com>
2017-12-10 22:12:31 -0600

    init commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import { AppState } from '../app.service';
+import { Title } from './title';
+import { XLargeDirective } from './x-large';
+import { Dimensions } from '../../classes/dimensions'
+
+@Component({
+  /**
+   * The selector is what angular internally uses
+   * for `document.querySelectorAll(selector)` in our index.html
+   * where, in this case, selector is the string 'home'.
+   */
+  selector: 'home',  // <home></home>
+  /**
+   * We need to tell Angular's Dependency Injection which providers are in our app.
+   */
+  providers: [
+    Title
+  ],
+  /**
+   * Our list of styles in our component. We may add more to compose many styles together.
+   */
+  styleUrls: [ './home.component.css' ],
+  /**
+   * Every Angular template is first compiled by the browser before Angular runs it's compiler.
+   */
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  /**
+   * Set our default values
+   */
+ dimensions: Dimensions;
+  /**
+   * TypeScript public modifiers
+   */
+  constructor(
+    public appState: AppState,
+    public title: Title
+  ) {
+    this.dimensions=new Dimensions();
+  }
+
+  public ngOnInit() {
+    console.log('hello `Home` component');
+    /**
+     * this.title.getData().subscribe(data => this.data = data);
+     */
+  }
+
+  public calculateDimensions() {
+  //  this.dimensions.height=1;
+  //  this.dimensions.width=1;
+  //  this.dimensions.length=1;
+   this.dimensions.calculated = 'Cut your paper to ';
+  }
+}
---
+++ src/classes/dimensions.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+export class Dimensions {
+    length: number;
+    width: number;
+    height: number;
+    calculated: string;
+}
